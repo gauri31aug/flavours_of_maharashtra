914400fd69786bc663cb1e209a52f1481d205e99
gauri31aug <gauri_31aug@yahoo.co.in>
2017-01-31 21:03:26 +0530

    Add files via upload

---
+++ source/salad_papad_pickle_chatni/chatni/karale.php
@@ -0,0 +1,93 @@
+<?php
+
+$servername="localhost";
+$username="gauri";
+$password="";
+$dbname="flavours_of_maharashtra";
+
+$conn=mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$conn)
+{
+  die("Connection failed:".mysqli_connect_error());
+}
+else
+{
+   $sql="select count(*) from cart";
+   
+   $result=mysqli_query($conn,$sql) or die("Query failed:".mysqli_error());
+  
+   $row=mysqli_fetch_row($result);
+  
+   $cart_count=$row[0];
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>karale</title>
+<link rel="stylesheet" href="../../../menu_item.css" type="text/css">
+<script src="../../../Scripts/jquery-1.7.1.js"></script>
+<script>
+$(document).ready(function(){
+	$("#descp").hide();
+	$("#hide_descp").hide();
+	$("#show_descp").click(function(){
+		$(this).hide();
+		$("#hide_descp").show();
+		$("#descp").show();
+	});
+	$("#hide_descp").click(function(){
+		$(this).hide();
+		$("#show_descp").show();
+		$("#descp").hide();
+	});
+});
+</script>
+</head>
+<body>
+<form action="../../../php/addtocart.php" method="post">
+<nav>
+<ul>
+<li><a href="../../../main.php">Home</a></li>
+<li><a href="../../../snacks.php">Snacks</a></li>
+<li class="dropdown">Thali
+  <ul class="dropdown-content">
+    <li><a href="../../../thali_veg.php">Veg</a></li><br>
+	<li><a href="../../../thali_non_veg.php">Non-Veg</a></li><br>
+	<li><a href="../../../breads.php">Breads</a></li><br>
+	<li><a href="../../../salad_papad_pickle_chatni.php">Salads/Papads/Pickles/Chatni</a></li>
+  </ul>
+</li>
+<li><a href="../../../beverages.php">Beverages</a></li>
+<li><a href="../../../sweets.php">Sweets</a></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li><a href="../../../php/view_cart.php">View Cart: </a><div style="display:inline-block; color:white; font-family:century gothic; font-style:italic; width:40px; height:25px; background-color:#c5c52e;text-align:center"><?php echo $cart_count; ?></div></li>
+</ul>
+</nav>
+
+<br>
+<figure>
+<img src="karale.jpg" />
+<figcaption>Karale Chatni</figcaption>
+<figcaption>1 tsp</figcaption>
+<figcaption>Rs.10</figcaption>
+</figure><br>
+<button name="item_name" value="karale">Add to Cart</button>
+<br><br>
+<div id="show_descp">What's this?</div>
+<div id="hide_descp">Hide</div>
+<div id="descp">Niger seeds are popularly known as "Karale" in Maharashtra. Owing to the high presence of essential nutrients, omega 3 fatty acids, dietary fibers and lignan it has been attributed with the tag of superfood. Karale Chatni is a very popular rustic Maharashtrian dish that makes for a delicious accompaniment with Bhakri or Chapati. Niger seeds are dry roasted in a pan until it releases a nutty aroma and a visible change in color of seeds, then combined with garlic, salt and red chilli powder for the extra zing.
+</div>
+</form>
+</body>
+</html>
