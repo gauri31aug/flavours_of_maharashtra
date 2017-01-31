bc348654c63651ed29b3203e953ca85e34a52ec1
gauri31aug <gauri_31aug@yahoo.co.in>
2017-01-31 21:01:02 +0530

    Add files via upload

---
+++ source/breads/rice_bhakri.php
@@ -0,0 +1,92 @@
+<?php
+
+$servername="localhost";
+$username="gauri";
+$password="";
+$dbname="flavours_of_maharashtra";
+
+$conn=mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$conn)
+{
+  die("Connection failed:".mysqli_connect_error());
+}
+else
+{
+   $sql="select count(*) from cart";
+   
+   $result=mysqli_query($conn,$sql) or die("Query failed:".mysqli_error());
+  
+   $row=mysqli_fetch_row($result);
+  
+   $cart_count=$row[0];
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>rice_bhakri</title>
+<link rel="stylesheet" href="../../menu_item.css" type="text/css">
+<script src="../../Scripts/jquery-1.7.1.js"></script>
+<script>
+$(document).ready(function(){
+	$("#descp").hide();
+	$("#hide_descp").hide();
+	$("#show_descp").click(function(){
+		$(this).hide();
+		$("#hide_descp").show();
+		$("#descp").show();
+	});
+	$("#hide_descp").click(function(){
+		$(this).hide();
+		$("#show_descp").show();
+		$("#descp").hide();
+	});
+});
+</script>
+</head>
+<body>
+<form action="../../php/addtocart.php" method="post">
+<nav>
+<ul>
+<li><a href="../../main.php">Home</a></li>
+<li><a href="../../snacks.php">Snacks</a></li>
+<li class="dropdown">Thali
+  <ul class="dropdown-content">
+    <li><a href="../../thali_veg.php">Veg</a></li><br>
+	<li><a href="../../thali_non_veg.php">Non-Veg</a></li><br>
+	<li><a href="../../breads.php">Breads</a></li><br>
+	<li><a href="../../salad_papad_pickle_chatni.php">Salads/Papads/Pickles/Chatni</a></li>
+  </ul>
+</li>
+<li><a href="../../beverages.php">Beverages</a></li>
+<li><a href="../../sweets.php">Sweets</a></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li><a href="../../php/view_cart.php">View Cart: </a><div style="display:inline-block; color:white; font-family:century gothic; font-style:italic; width:40px; height:25px; background-color:#c5c52e;text-align:center"><?php echo $cart_count; ?></div></li>
+</ul>
+</nav>
+
+<br>
+<figure>
+<img src="rice_bhakri.jpg" />
+<figcaption>Tandul Bhakri</figcaption>
+<figcaption>2 pcs</figcaption>
+<figcaption>Rs.30</figcaption>
+</figure><br>
+<button name="item_name" value="rice_bhakri">Add to Cart</button>
+<br><br>
+<div id="show_descp">What's this?</div>
+<div id="hide_descp">Hide</div>
+<div id="descp">Tandalachi bhakri is prepared using rice flour. It is very popular in coastal part of Maharashtra where rice is grown abundantly. It goes really well with non-veg curries like chicken, fish, mutton as well as veg dishes.
+</div>
+</form>
+</body>
+</html>
